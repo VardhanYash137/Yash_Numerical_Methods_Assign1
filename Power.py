169c6d8c0e460b94efddc82c599e259e7f013dbc
Yash Vardhan <135862777+VardhanYash137@users.noreply.github.com>
2023-11-20 14:09:11 +0530

    Use membership expression with in operator in Power.py

--- Power.py
+++ Power.py
@@ -7,12 +7,11 @@
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-for i, value in enumerate(L):
-    if 2 ** X == value:
-        print('at index', i)
-        break
+if 2 ** X in L:
+    print('at index', L.index(2 ** X))
 else:
     print(X, 'not found')
+
 
 
 
