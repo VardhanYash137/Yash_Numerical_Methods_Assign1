316ae2a6d0571eb9d16257730b6aa3d36949b7e4
Yash Vardhan <135862777+VardhanYash137@users.noreply.github.com>
2023-11-20 14:10:18 +0530

    Generate powers-of-2 list using a for loop and list append method in Power.py

--- Power.py
+++ Power.py
@@ -4,13 +4,14 @@
 # In[3]:
 
 
-L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
+L = [2 ** i for i in range(X + 1)]
 
 if 2 ** X in L:
     print('at index', L.index(2 ** X))
 else:
     print(X, 'not found')
+
 
 
 
